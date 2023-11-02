ede64512dbca1e241c56c6f91e7f48d25d9fb759
lukman <lukman@lukmans-MacBook-Air.local>
2023-11-02 11:15:53 +0800

    feat : modules index
    Deskripsi:
    - tambah index modules
    
    No Issue

---
+++ src/modules/index.ts
@@ -0,0 +1,54 @@
+import ListActivities from "./activities/view/list_activities";
+import DetailArchive from "./archive/view/detail_archive";
+import ListArchive from "./archive/view/list_archive";
+import DetailAssets from "./assets/view/detail_assets";
+import DetailAssetsHistory from "./assets/view/detail_assets_history";
+import DetailAssetsViewCode from "./assets/view/detail_assets_view_code";
+import EditAssets from "./assets/view/edit_assets";
+import ListAssets from "./assets/view/list_assets";
+import ViewListAddAssets from "./assets/view/view_add_assets";
+import { DetailDeliveryStatus } from "./delivery_status/view/detail_delivery_status";
+import ListDeliveryStatus from "./delivery_status/view/list_delivery_status";
+import ListDraftProject from "./draft_project/view/list_draft_project";
+import ListAddAssets from "./project/view/list_add_assets";
+import TabelListClient from "./project/view/list_client";
+import ListCreateProject from "./project/view/list_create_project";
+import ListDetailAddAssets from "./project/view/list_detail_add_assets";
+import ListEditProject from "./project/view/list_edit_project";
+import ListUpdateProject from "./project/view/list_update_project";
+import ListViewDetailAssets from "./project/view/list_view_detail_assets";
+import ViewHistoryScan from "./project/view/view_history_scan";
+import ViewScanBarcode from "./project/view/view_scan_barcode";
+import { DetailProjectStatus } from "./project_status/view/detail_project_status";
+import ListProjectStatus from "./project_status/view/list_project_status";
+import ListCreateIdQuotation from "./quotation/view/list_create_id_quotation";
+import ListCreateQuotation from "./quotation/view/list_create_quotation";
+import ListQuotation from "./quotation/view/list_quotation";
+import ListRecentActivity from "./recent_activity/view/list_recent_activity";
+
+export {ListCreateQuotation}
+export {ListCreateIdQuotation}
+export {ListQuotation}
+export {DetailArchive}
+export {ListArchive}
+export {ListActivities}
+export {DetailDeliveryStatus}
+export {ListDeliveryStatus}
+export {ListEditProject}
+export {ListProjectStatus}
+export {ListUpdateProject}
+export {DetailProjectStatus}
+export {ListDraftProject}
+export {ListRecentActivity}
+export {ListViewDetailAssets}
+export {ListDetailAddAssets}
+export {ListCreateProject}
+export {TabelListClient}
+export {ListAssets}
+export {DetailAssets}
+export {DetailAssetsViewCode}
+export {DetailAssetsHistory}
+export {EditAssets}
+export {ViewListAddAssets}
+export {ViewScanBarcode}
+export {ViewHistoryScan}
